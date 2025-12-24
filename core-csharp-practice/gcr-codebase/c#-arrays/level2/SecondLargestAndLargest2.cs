74977c9a25508ea17abb030f0258838269b25212
Pushpendra Upadhyay <pushpendraupadhyay2004@gmail.com>
2025-12-24 15:30:21 +0530

    [Pushpendra Upadhyay] Add . Solving the practice problems

---
+++ core-csharp-practice/gcr-codebase/c#-arrays/level2/SecondLargestAndLargest2.cs
@@ -0,0 +1,49 @@
+using System;
+
+class SecondLargestAndLargest2
+{
+    static void Main()
+    {
+        Console.Write("Enter number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        int maxDigit = 10;
+        int[] digits = new int[maxDigit];
+        int index = 0;
+
+        while (number != 0)
+        {
+            if (index == maxDigit)
+            {
+                maxDigit += 10;
+                int[] temp = new int[maxDigit];
+
+                for (int i = 0; i < digits.Length; i++)
+                    temp[i] = digits[i];
+
+                digits = temp;
+            }
+
+            digits[index++] = number % 10;
+            number /= 10;
+        }
+
+        int largest = 0, secondLargest = 0;
+
+        for (int i = 0; i < index; i++)
+        {
+            if (digits[i] > largest)
+            {
+                secondLargest = largest;
+                largest = digits[i];
+            }
+            else if (digits[i] > secondLargest && digits[i] != largest)
+            {
+                secondLargest = digits[i];
+            }
+        }
+
+        Console.WriteLine("Largest digit: " + largest);
+        Console.WriteLine("Second Largest digit: " + secondLargest);
+    }
+}
