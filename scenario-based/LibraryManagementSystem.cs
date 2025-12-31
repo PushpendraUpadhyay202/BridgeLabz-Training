3f5a58513871abd32cab0c45ea342e6c51bf7ecc
Pushpendra Upadhyay <pushpendraupadhyay2004@gmail.com>
2025-12-31 11:01:27 +0530

    [Pushpendra Upadhyay] Add . Solving scenario based problems

--- scenario-based/LibraryManagementSystem.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-public class LibraryManagementSystem
-{
-    public static void Main(string[] args)
-    {
-        Console.WriteLine("Getting ready to solve this problems");
-    }
-}
---
+++ scenario-based/bankaccountmanager/Bank.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class Bank
+{
+    public static void Main(string[] args)
+    {
+        
+    }
+}
