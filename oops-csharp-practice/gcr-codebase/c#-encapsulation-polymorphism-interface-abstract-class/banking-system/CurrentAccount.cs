f040e2bee0a1da5320153c05307d47c65fdd0566
Pushpendra Upadhyay <pushpendraupadhyay2004@gmail.com>
2026-01-07 23:04:42 +0530

    [Pushpendra Upadhyay] Add . Solving the practice problem

---
+++ oops-csharp-practice/gcr-codebase/c#-encapsulation-polymorphism-interface-abstract-class/banking-system/CurrentAccount.cs
@@ -0,0 +1,9 @@
+﻿internal class CurrentAccount : BankAccount
+{
+    public CurrentAccount(string accountNumber, string holderName, double balance, string pin): base(accountNumber, holderName, balance, pin) { }
+
+    public override double CalculateInterest()
+    {
+        return 0;
+    }
+}
---
+++ oops-csharp-practice/gcr-codebase/c#-encapsulation-polymorphism-interface-abstract-class/banking-system/SavingsAccount.cs
@@ -0,0 +1,19 @@
+﻿internal class SavingsAccount : BankAccount, ILoanable
+{
+    public SavingsAccount(string accountNumber, string holderName, double balance, string pin): base(accountNumber, holderName, balance, pin) { }
+
+    public override double CalculateInterest()
+    {
+        return balance * 0.04;
+    }
+
+    public void ApplyForLoan()
+    {
+        Console.WriteLine("Loan application submitted for Savings Account.");
+    }
+
+    public double CalculateLoanEligibility()
+    {
+        return balance * 2;
+    }
+}
