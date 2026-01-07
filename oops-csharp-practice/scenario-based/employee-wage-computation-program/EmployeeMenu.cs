50f79135e48db7e3e25f3ca8dd60bfc74e354924
Pushpendra Upadhyay <pushpendraupadhyay2004@gmail.com>
2026-01-07 16:32:48 +0530

    [Pushpendra Upadhyay] Add . Version one(UC 1 : Check Employee is Present or Absent)

---
+++ oops-csharp-practice/scenario-based/employee-wage-computation-program/EmployeeMenu.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeManagement
+{
+    sealed class EmployeeMenu
+    {
+        private IEmployee employeeUtility;
+        public void Menu()
+        {
+            employeeUtility = new EmployeeUtilityImpl();
+            Console.WriteLine("\n-------   Welcome to Employee Wage Computation Program    -------");
+            while (true)
+            {
+                Console.WriteLine("0.Exit");
+                Console.WriteLine("1.Add Employee");
+                Console.WriteLine("2. Check employee Attendance");
+                int choice = int.Parse(Console.ReadLine());
+                switch(choice)
+                {
+                    case 0:
+                        Console.WriteLine("Program Exiting......");
+                        return;
+                    case 1:
+                        if(EmployeeMain.count < EmployeeMain.employees.Length)
+                        {
+                            Employee employee = employeeUtility.AddEmployee();
+                            EmployeeMain.employees[EmployeeMain.count++] = employee;
+                            Console.WriteLine("Employee Added Successfully");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Employee Array is full");
+                        }
+                            break;
+                    case 2:
+                        bool employeeAttendance = employeeUtility.CheckEmployeeAttendance();
+                        Console.WriteLine("Attendance: " + employeeAttendance + "\n");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Input");
+                        Console.WriteLine("Enter option in between 0 to 4 only");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ oops-csharp-practice/scenario-based/employee-wage-computation-program/EmployeeUtilityImpl.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeManagement
+{
+    internal class EmployeeUtilityImpl : IEmployee
+    {
+        private int EmployeeId = 4;
+
+        public Employee AddEmployee()
+        {
+            Console.Write("Enter Employee Name: ");
+            string employeeName = Console.ReadLine();
+
+            Console.Write("Enter employee attendance (true/false): ");
+            bool employeeAttendance = bool.Parse(Console.ReadLine());
+
+            Console.Write("Enter wage per hour: ");
+            double wagePerHour = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter employee type: ");
+            string employeeType = Console.ReadLine();
+
+
+            Employee employee = new Employee(
+                ++EmployeeId,
+                employeeName,
+                wagePerHour,
+                employeeAttendance,
+                employeeType
+            );
+            return employee;
+        }
+
+        //UC 1 : Check Employee is Present or Absent
+        public bool CheckEmployeeAttendance()
+        {
+            Console.Write("Enter Employee ID: ");
+            int employeeId = int.Parse(Console.ReadLine());
+
+            foreach(Employee employee in EmployeeMain.employees)
+            {
+                if(employee != null && employee.GetEmployeeId() == employeeId)
+                {
+                    return employee.GetIsPresent();
+                }
+            }
+            Console.WriteLine("Employee Doesn't exist\n");
+            return false;
+        }
+    }
+
+}
