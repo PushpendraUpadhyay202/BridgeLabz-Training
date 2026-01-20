1f7c27f6fb28b32be2a559b7ac2f9abf997c6594
Pushpendra Upadhyay <pushpendraupadhyay2004@gmail.com>
2026-01-20 16:31:38 +0530

    [Pushpendra Upadhyay] Add . Added Password Cracker Simulator Scenario Problem.

---
+++ dsa-csharp-practice/scenario-based-codebase/password-cracker-simulator/PasswordCrackerMain.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Text;
+
+namespace Password_Cracker_Simulator
+{
+    internal class PasswordCrackerMain
+    {
+        public static void Main(string[] args)
+        {
+            PasswordCrackerMenu menu = new PasswordCrackerMenu();
+            menu.Menu();
+        }
+    }
+}
