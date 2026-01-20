2e02baadc2b1300bed68d405bdda723848155da4
Pushpendra Upadhyay <pushpendraupadhyay2004@gmail.com>
2026-01-20 15:33:10 +0530

    [Pushpendra Upadhyay] Add . Added Adhar Number System Scenario Problem.

---
+++ dsa-csharp-practice/scenario-based-codebase/adhar-number-system/Adhar.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Adhar_Number_Sorting
+{
+    internal class Adhar
+    {
+        private string adharId;
+        private string adharName;
+
+        public Adhar(string id , string name)
+        {
+            adharId = id;
+            adharName = name;
+        }
+
+        public string GetAdharId()
+        {
+            return adharId;
+        }
+
+        public string GetAdharName()
+        {
+            return adharName;
+        }
+
+        public override string? ToString()
+        {
+            return "Adhar Id : "+adharId+" Adhar Name : "+adharName;
+        }
+    }
+}
---
+++ dsa-csharp-practice/scenario-based-codebase/adhar-number-system/AdharNumberMain.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Adhar_Number_Sorting
+{
+    internal class AdharNumberMain
+    {
+        public static void Main(string[] args)
+        {
+            AdharMenu menu = new AdharMenu();
+            menu.Menu();
+        }
+    }
+}
